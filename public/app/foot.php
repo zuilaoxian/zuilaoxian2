32a2ed2d8b263b6532e615f94ff737c5f5c727ff
echozuilaoxian <602406964@qq.com>
2022-03-04 21:59:45 +0800

    update 20220304

--- public/app/foot.php
+++
@@ -1,7 +0,0 @@
-<?php
-$weekarray=["日","一","二","三","四","五","六"];
-$html_end='
-	<div class="well well-sm"><span id="times" class="glyphicon glyphicon-time">'.date('Y-m-d H:i:s').' 星期'.$weekarray[date("w")].'</span></div>
-</div>
-</body>
-</html>';
